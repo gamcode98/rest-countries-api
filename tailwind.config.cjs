1bab3e6072aba4a69cbcbb58bf5c4193065fb671
gamcode98 <gamcode98@gmail.com>
2023-03-01 09:38:27 -0300

    add: colors settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    fontFamily: {
-      'nunito-sans' : ['Nunito Sans', 'sans-serif']
+    colors: {
+      'dm-dark-blue': 'hsl(209, 23%, 22%)',
+      'dm-very-dark-blue': 'hsl(207, 26%, 17%)',
+      'lm-very-dark': 'hsl(200, 15%, 8%)',
+      'lm-dark-gray': 'hsl(0, 0%, 52%)',
+      'lm-very-light-gray': 'hsl(0, 0%, 98%)',
+      white: 'hsl(0, 0%, 100%)'
     },
-    extend: {},
+    fontFamily: {
+      'nunito-sans': ['Nunito Sans', 'sans-serif']
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
